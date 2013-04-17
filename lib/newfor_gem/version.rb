998524b1c4cd3a6dbff1ef8bbfe69e97fadce3f6
Nigel Blackmore <nigel@blackmore.de>
2013-04-17 16:08:04 +0200

    Version Change 0.6.1 removed old files and mod file names

--- lib/newfor_gem/version.rb
+++ lib/newfor_gem/version.rb
@@ -1,3 +1,3 @@
 module NewforGem
-  VERSION = "0.2.0"
+  VERSION = "0.6.1"
 end
--- spec/newfor_spec.rb
+++ spec/newfor_spec.rb
@@ -1,100 +1,59 @@
 # -*- encoding: utf-8 -*-
 require 'minitest/spec'
 require 'minitest/autorun'
+#require 'minitest/wscolor'
 
-require "json"
-require "newfor_gem"
+require 'json'
+require 'newfor_gem'
+require 'newfor_gem/newfor'
+require 'newfor_gem/hamming'
 
-describe NewforGem do
-  CLEAR               = "\x18"
-  BUILD               = "\x0F\x0C\x02\x64\x20\x20\x0D\x07\x0B\x0B\x31\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x0A\x0A\x20\x20\x20\x20\x20\x02\x38\x20\x20\x0D\x07\x0B\x0B\x32\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x2E\x0A\x0A\x20\x20\x20\x20"
-  GERMAN_BUILD        = "\x0F\x0C\x02\x64\x20\x20\x0D\x07\x0B\x0B\x31\x61\x62\x63\x64\x5d\x5d\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x0A\x0A\x20\x20\x20\x20\x20\x02\x38\x20\x20\x0D\x07\x0B\x0B\x32\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x2E\x0A\x0A\x20\x20\x20\x20"
-  PACKET_26           = "\x0f\x0c\x02\x0c\x15\x79\x93\x00\x1e\xbd\xd0\x25\x6d\x43\x3e\xd1\x4e\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\x7f\xff\x02\x38\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0d\x07\x0b\x0b\x53\x31\x34\x20\xa0\x20\xa0\x20\xa0\x2e\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20" 
+describe NewforGem::Newfor do
+	CLEAR        = "\x18"
+	BUILD        = "\x0F\x0C\x02\x64\x20\x20\x0D\x07\x0B\x0B\x31\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x0A\x0A\x20\x20\x20\x20\x20\x02\x38\x20\x20\x0D\x07\x0B\x0B\x32\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x2E\x0A\x0A\x20\x20\x20\x20"
+  PX_26        = "\x0f\x0c\x02\x0c\x15\x79\x93\x00\x1e\xbd\xd0\x25\x6d\x43\x3e\xd1\x4e\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\xff\x80\x74\x7f\xff\x02\x38\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0d\x07\x0b\x0b\x53\x31\x34\x20\xa0\x20\xa0\x20\xa0\x2e\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20" 
+  GERMAN_BUILD = "\x0F\x0C\x02\x64\x20\x20\x0D\x07\x0B\x0B\x31\x61\x62\x63\x64\x5d\x5d\x67\x68\x69\x6A\x6B\x6C\x6D\x6E\x6F\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7A\x0A\x0A\x20\x20\x20\x20\x20\x02\x38\x20\x20\x0D\x07\x0B\x0B\x32\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4A\x4B\x4C\x4D\x4E\x4F\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5A\x2E\x0A\x0A\x20\x20\x20\x20"
+  ES_PKT_S2    = "\x0f\x47\x02\x49\x20\x0d\x07\x0b\x0b\x53\x32\x20\x71\x77\x65\x72\x74\x79\x75\x69\x6f\x70\x61\x73\x64\x66\x67\x68\x6a\x6b\x6c\x7c\x7a\x78\x63\x76\x62\x6e\x6d\x0a\x0a\x20\x20\x20"
+  KLIVE        = "\x8f\x02\x02\x38\x0d\x07\x0b\x0b\x61\x6e\x73\xf7\xe5\xf2\x20\xf4\xef\x20\xf4\x68\xe9\x73\xae\x20\xd0\x68\xe9\xec\x8a\x8a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x10"
+  TWO_LINE     = "\x0f\x49\x02\x64\x0d\x07\x0b\x0b\x41\x6c\x6c\x20\x77\x6f\x72\x6b\x20\x61\x6e\x64\x20\x6e\x6f\x20\x70\x6c\x61\x79\x20\x6d\x61\x6b\x65\x73\x20\x4a\x61\x63\x6b\x20\x61\x0a\x0a\x20\x02\x38\x0d\x07\x0b\x0b\x64\x75\x6c\x6c\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20"
 
-  it "should return true on clear data" do
-    obj = NewforGem.parse(CLEAR)
-    obj['code'].must_equal "clear"
+  def read_obj(j)
+    NewforGem::Newfor.read(j)
   end
-  
-  # it "should return true to a build data type" do
-  #   obj = NewforGem::Newfor.read(BUILD)
-  #   obj.is_build_data?.must_equal true
-  #   obj.is_clear_data?.must_equal false  
-  # end
-  
-  # it "should return a bindata" do
-  #   obj = NewforGem::Newfor.read(BUILD)
-  #   obj.rows[1].text.must_equal "  \r\a\v\v2ABCDEFGHIJKLMNOPQRSTUVWXYZ.\n\n    "
-  # end
-  
-  # it "should return a clear hash" do
-  #   obj = NewforGem::Newfor.read(CLEAR)
-  #   obj.clean(nil).to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"clear\"\}/)
-  # end
-  
-  # it "should not parse str as German" do
-  #   op = NewforGem.parse(BUILD)
-  #   op['code'].wont_equal "clear"
-  # end
-  
-  # it "should not parse str as German" do
-  #   op = NewforGem.parse(CLEAR)
-  #   op['code'].must_equal "clear"
-  # end
-  
-  # it "should not parse str as German" do
-  #   op = NewforGem.parse(CLEAR, "DE")
-  #   op['code'].must_equal "clear"
-  # end
-  
-  # it "should not parse str as German" do
-  #   op = NewforGem.parse(BUILD, "DE")
-  #   op['code'].must_equal "build"
-  # end
 
-  # it "should build string" do
-  #   op = NewforGem.parse(KLIVE)
-  #   op['code'].must_equal "build"
-  # end
+  def parse_obj(j)
+    NewforGem.parse(j)
+  end
 
-  # it "should return a build json string" do
-  #   obj = NewforGem::Newfor.read(BUILD)
-  #   obj.clean(nil).to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"build\",\"rows\":\[\"1abcdefghijklmnopqrstuvwxyz\",\"2ABCDEFGHIJKLMNOPQRSTUVWXYZ.\"\]}/)
-  # end
+	it "must return a Clear package type" do
+    read_obj(CLEAR)[:package_type].must_equal 0x18
+    obj = parse_obj(CLEAR)
+    obj['code'].must_equal "clear"
+    obj['rows'].must_be_nil
+  end
 
-  # it "Should return two lines" do
-  #   obj = NewforGem::Newfor.read(TWO_LINE)
-  #   obj.clean(nil).to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"build\",\"rows\":\[\"All work and no play makes Jack a\",\"dull\"\]}/)
-  # end
+  it "must return a Build package type with two text lines" do
+  	read_obj(BUILD)[:package_info].must_equal 0x0c
+    obj = parse_obj(BUILD)
+    obj['code'].must_equal "build"
+    obj['rows'].must_be_instance_of Array
+    obj['rows'].length.must_equal 2
+  end
 
-  # it "should return a build json string" do
-  #   obj = NewforGem.ensure_odd_parity(KLIVE)
-  #   ob = NewforGem::Newfor.read(obj)
-  #   ob.clean(nil).to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"build\",\"rows\":\[\"answer to this. Phil\"\]}/)
-  # end
-  
-  # it "should return a correct german string" do
-  #   obj = NewforGem::Newfor.read(GERMAN_BUILD)
-  #   obj.clean("DE").to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"build\",\"rows\":\[\"1abcdÜÜghijklmnopqrstuvwxyz\",\"2ABCDEFGHIJKLMNOPQRSTUVWXYZ.\"\]}/ )
-  # end
-  
-  # it "should return a correct English string" do
-  #   obj = NewforGem::Newfor.read(GERMAN_BUILD)
-  #   obj.clean("EN").to_json.must_match(/\{\"timestamp\":\"\d\d:\d\d:\d\d:\d\d\d\",\"code\":\"build\",\"rows\":\[\"1abcd\]\]ghijklmnopqrstuvwxyz\",\"2ABCDEFGHIJKLMNOPQRSTUVWXYZ.\"\]}/)
-  # end
-  
-  # it "should parse str as German" do
-  #   op = NewforGem.parse(GERMAN_BUILD, "DE")
-  #   op['rows'][0].must_equal "1abcdÜÜghijklmnopqrstuvwxyz"
-  # end
+  it "must retrun length of x26 and mst be 13" do
+    obj = read_obj(PX_26)
+  	obj.packet[0].address1.must_equal 12
+  end
 
-  # it "should parse str as German" do
-  #   op = NewforGem.parse(KLIVE)
-  #   op['rows'][0].must_equal "answer to this. Phil"
-  # end
-  
-  # it "should not parse str as German" do
-  #   op = NewforGem.parse(GERMAN_BUILD)
-  #   op['rows'][0].wont_equal "1abcdÜÜghijklmnopqrstuvwxyz"
-  # end
+  it "must return the chrs for German" do
+    obj = NewforGem.parse(GERMAN_BUILD, "DE")
+    obj['rows'][0].must_include "Ü"
+  end
+
+  it "wont include Ü but will default to EN »" do
+    obj = NewforGem.parse(GERMAN_BUILD)
+    obj['rows'][0].wont_include "Ü"
+    obj['rows'][0].must_include "»"
+  end
+
 end
